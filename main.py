b144c23f56b4c8cbbb57bbc476f30b5977c6887c
Luis Martinez <s_k_ap3@hotmail.com>
2021-12-31 01:58:46 -0600

    terminando clase de modelos

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+#python
+from typing import Optional
+#pydantic
+from pydantic import BaseModel
+#FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+#models
+class Person(BaseModel):
+    name: str 
+    a_paterno: str
+    a_materno: str
+    age: Optional[int] = 0
+    user_name: str
+    email: str
+    is_active: Optional[bool] = False
 
 @app.get("/")
 def home():
     return {"hello": "word"}
+
+#reques an response body
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
